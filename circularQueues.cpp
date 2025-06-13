90cf1ed6aeb095226c24998a926a1a29b7f7e1e5
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-06-13 08:28:14 +0700

    Mengecek apakah antrian kosong pada prosedur remove

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -37,4 +37,15 @@ class Queues{
         }
         queue_array[REAR] = num;
     }
+
+    void remove(){
+        //cek apakah antrian kosong
+        if (FRONT == -1){
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: "<< queue_array[FRONT]<< endl;
+
+       
+    }
 };
