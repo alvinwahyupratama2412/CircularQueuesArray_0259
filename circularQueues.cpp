a9cfc209e3e1cf553f980189cf0a98c2aa8064b5
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-06-13 08:15:27 +0700

    deklarasi variabel num dan membuat tampilan input num pada prosedur insert

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -11,5 +11,12 @@ class Queues{
         REAR  = -1;
     }
 
-   
+    void insert(){
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+       
+    }
 };
