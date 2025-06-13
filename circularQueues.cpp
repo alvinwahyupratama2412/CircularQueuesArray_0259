6da99b5362a4e87362c2a06c653f26fe74e03bc6
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-06-13 08:30:05 +0700

    else jika element yang dihapus berada diposisi terakhir array, kembali ke awal array

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -46,6 +46,16 @@ class Queues{
         }
         cout << "\nThe element deleted from the queue is: "<< queue_array[FRONT]<< endl;
 
-       
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR){
+            FRONT = -1;
+            REAR = -1;
+        }
+        else{
+            //jika element yang dihapus berada diposisi terakhir array, kembali ke awal array
+            if (FRONT == max -1)
+            FRONT=0;
+            else FRONT = FRONT +1;
+        }
     }
 };
